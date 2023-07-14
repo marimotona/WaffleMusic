16b78761af571268fb678bf254ad02d3d4462e2e
marimotona <marimotona>
2023-07-14 19:39:26 +0900

    不要なコメントの削除

--- music/app.js
+++ music/app.js
@@ -2,78 +2,27 @@ const express = require("express");
 //require("dotenv").config({ debug: true });
 const bodyParser = require('body-parser');
 const path = require('path');
-
 let app = express();
-const queue = [];
 
 
 app.use('/music', express.static(path.join(__dirname, 'view')));
-app.use('/music', express.static(path.join(__dirname, 'images')));
-//app.use('/wafflemusic', express.static(path.join(__dirname, 'music')));
 app.use(bodyParser.json());
-
-
-//apple music authorization
-let music = MusicKit.getInstance();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'view', 'home.html'));
 });
 
-
-//apple musicから曲を検索する関数
-async function searchMusic(word) {
-    const res = await fetch(`https://api.music.apple.com/v1/catalog/jp/search?term=${word}&types=songs&limit=10`);
-    if(!res.ok) {
-        console.log(`error search : ${word}`, res.status, res.statusText);
-    }
-
-    const getData = await res.json();
-    console.log(getData);
-
-    const musicBank = getData.result.songs.data;
-    musicBank.forEach(ele => {
-        queue.push(ele.id);
-    });
-
-    if(queue.length === musicBank.length) {
-        nextplayMusic();
-    }
-
-    res.status(200);
-    return queue;
+const queue = [];
+async function getmusicStack(word) {
+    // const queue = [];
+    queue.push(word);
+    //console.log(queue);
 }
 
-
-//musicが再生し終わったら、要素の先頭を削除する
-async function nextplayMusic() {
-    if(queue.length > 0) {
-        const trackID = queue[0];
-        console.log(`now on playing ${trackID}`);
-
-        //const res = await fetch(`https://api.music.apple.com/v1/catalog/jp/search?term=${word}&types=songs&limit=10`);
-        
-        if(!res.ok) {
-            console.log("error", res.status, res.statusText);
-        }
-
-        music.authorize().then(function() {
-            music.player.play();
-            queue.shift();
-            nextplayMusic();
-          });
-        // res.body.on("playend", () => {
-        //     console.log(`finish song : ${trackID}`);
-        //     queue.shift();
-        //     nextplayMusic();
-        // });
-    }
-}
-
-
-app.get("/play", async (req, res) => {
+app.get("/search", async (req, res) => {
     const word = req.query.word;
-    searchMusic(word);
+    const musicBank = await getmusicStack(word);
+    console.log(musicBank);
 });
 
 
--- music/view/index.js
+++ music/view/index.js
@@ -1,16 +1,13 @@
 console.log("index.js");
-const wordID = document.getElementById("word");
-
 
 async function stackMusic() {
     const searchWord = document.getElementById("word").value;
     console.log(searchWord);
     const searchData = await fetch(
-        `http://localhost:3000/play?word=${searchWord}`
+        `http://localhost:3000/search?word=${searchWord}`
     ).then(res => res.json());
 
     console.log(searchData);
-    wordID.reset();
     return searchData;
 }
 
